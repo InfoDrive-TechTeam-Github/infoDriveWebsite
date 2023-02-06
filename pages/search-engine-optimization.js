d348c9f2caa45045be79c1e6ff3540f65078451b
Dung Nguyen <dung.nguyen@mindatlas.com>
2023-02-06 11:04:05 +0700

    update digital and search optimize

---
+++ pages/search-engine-optimization.js
@@ -0,0 +1,268 @@
+import * as React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import utilStyles from '../styles/utils.module.css';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+//import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Avatar from '@mui/material/Avatar';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import Link from '@mui/material/Link';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Stack from '@mui/material/Stack';
+import Head from 'next/head';
+
+import LeadForm from '../components/leadForm';
+export default function SalesForceDevelopment() {
+  return (
+    <div>
+      <Head>
+        <title>
+          Top Android App Development Company & Developers in Singapore,
+          Malaysia, and India
+        </title>
+        <meta
+          name='description'
+          content='At InfoDrive Solutions, we specialize in developing high-quality android applications for businesses and organizations across Singapore, Malaysia, and India. With a team of experienced and skilled android developers.'
+        />
+        <meta
+          name='keywords'
+          content='Android App Development Company, Android App Development Company in Singapore, Android App Development Company in Malaysia, Android App Development Company in Kuala Lumpur, Android App Development Company in Bangalore, Top Android App Development Company in Singapore, Top Android App Development Company in Malaysia, Android App Developers in Singapore, Top Android App Developers in Malaysia, Android App Developers in India, Android App Developers in Bangalore, Mobile App Development Services in Singapore, Mobile App Development Services in Malaysia, Android App Development'
+        />
+        <link
+          rel='canonical'
+          href='https://infodrive-solutions.com/android-app-development.html'
+        />
+        <meta
+          property='og:title'
+          content='Top Android App Development Company & Developers in Singapore,Malaysia, and India'
+        />
+        <meta
+          property='og:image'
+          content='https://c5cea5.n3cdn1.secureserver.net/wp-content/uploads/2020/09/INFORDRIVE-LOGO-FINAL-01-1-1-1-1.png'
+        />
+        <link rel='icon' href='/favicon.png' />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=GTM-MB38MVS'+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MB38MVS');`,
+          }}
+        />
+      </Head>
+      <noscript>
+        <iframe
+          src='https://www.googletagmanager.com/ns.html?id=GTM-MB38MVS'
+          height='0'
+          width='0'
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      <Header />
+      <div className='sliderBox'>
+        <img
+          style={{ width: '100%' }}
+          src='images/8472ccba2259312408b9731d8ee9fcd1.jpeg'
+        />
+        <Box sx={{ flexGrow: 1 }} className='sliderContent '>
+          <Grid container spacing={0}>
+            <Typography gutterBottom variant='h3' component='div'>
+            Search Engine Optimization
+            </Typography>
+
+            <Typography gutterBottom variant='h1' component='div'>
+            Our team of experienced professionals is <br/>
+            dedicated to helping businesses of all <br/>
+            sizes improve their online presence and <br/>
+            drive more traffic to their websites <br/>
+            through effective search engine <br/>
+            optimization (SEO) strategies.
+            </Typography>
+            <Button
+              href='/contact-us.html'
+              className='readmore white normalCase'
+              variant='text'
+            >
+              Let's Talk <ArrowRightAltIcon />
+            </Button>
+            <nav className='socical-network'>
+              <List>
+                <ListItem>
+                  <ListItemButton
+                    component='a'
+                    href='https://www.facebook.com/InfoDrivesolutions/'
+                  >
+                    <ListItemIcon>
+                      <FacebookIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton
+                    component='a'
+                    href='https://in.linkedin.com/company/infodrive-solutions/'
+                  >
+                    <ListItemIcon>
+                      <LinkedInIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton
+                    component='a'
+                    href='https://www.instagram.com/infodrivesolutions/'
+                  >
+                    <ListItemIcon>
+                      <InstagramIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </nav>
+          </Grid>
+        </Box>
+      </div>
+
+      <section className={`sectionBox`}>
+        <Typography gutterBottom variant='h1' component='h1'>
+          InfoDrive Solutions is one of the best SEO Company in Singapore, Malaysia, and India. As a company that has been providing SEO services for many years, we have a deep understanding of how search engines work and what it takes to rank highly in search results. We use our expertise to develop customized SEO plans that are tailored to the unique needs of each of our clients.
+        </Typography>
+
+        <Typography className='w100' gutterBottom variant='h5' component='div'>
+          We pride ourselves on our transparent approach to SEO. We believe that it is important for our clients to understand the strategies that we are using to improve their online presence, which is why we provide regular updates and reports on the progress of our work.
+          <br/>
+          One of the key benefits of working with us is that we use only white hat SEO techniques. This means that we follow all of the guidelines set out by search engines such as Google, and we do not engage in any unethical or spammy practices. This not only helps to ensure that our clients see long-term, sustainable results, but it also protects their websites from being penalized by search engines.
+        </Typography>
+      </section>
+
+      <section
+        className={`sectionBox whyUsBox serviceOffer pb0 salesForceServices`}
+      >
+        <Typography className='w100 mb0 pb10' gutterBottom variant='h2' component='div'>
+        In addition to our core SEO services, we also offer a range of related services that can help businesses to improve their online presence and reach their target audience. These include social media marketing, content marketing, and local SEO services.  
+        </Typography>
+        <br />
+        <br />
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <Typography
+                    className='pr30'
+                    variant='body2'
+                    color='text.secondary'
+                  >
+                  Social media marketing is a powerful tool for businesses of all sizes, as it allows them to connect with their customers and build brand awareness in a more personal and engaging way. At InfoDrive Solutions, we have a team of social media experts who can help businesses to create and manage effective social media campaigns that drive traffic to their websites and help to convert leads into customers.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <Typography
+                    className='pr30'
+                    variant='body2'
+                    color='text.secondary'
+                  >
+                  Content marketing is another important aspect of digital marketing, as it helps businesses to showcase their expertise and build trust with their target audience. At InfoDrive Solutions, we have a team of experienced content writers who can help businesses to create high-quality, engaging content that is optimized for search engines and resonates with their target audience.
+                  Local SEO is an important consideration for businesses that operate in specific geographic locations. 
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+          </Grid>
+        </Box>
+      </section>
+
+      <br />
+      <br />
+
+      <section className={`sectionBox whyUsBox pt0 pb0 salesForceServices2`}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <img
+                    style={{ width: '100%' }}
+                    src='images/3f41e8b470a19936da4c15fc5a17ebf3.jpeg'
+                  />
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h2'
+                    component='div'
+                    className='w100 pl30'
+                  >
+                    By optimizing their websites and online profiles for local search terms, businesses can increase their visibility and attract more customers from their local area. At InfoDrive Solutions, we have a team of local SEO experts who can help businesses to optimize their online presence for local search and reach their target audience in their specific location.
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    color='text.secondary'
+                    className='pl30'
+                  >
+                    One of the things that sets InfoDrive Solutions apart from other SEO companies is our commitment to delivering results for our clients. We are not interested in making short-term gains or engaging in tactics that will only provide temporary boosts in traffic. Instead, we focus on developing long-term SEO strategies that drive sustainable growth and help businesses to achieve their goals.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </section>
+
+      <section className={`sectionBox connectUs backDrop mb0`}>
+        <Typography
+          gutterBottom
+          variant='h3'
+          className='white pb15 pt15 poppin'
+          component='div'
+        >
+          Take Your Business to the Next Level with Custom SaaS Solutions.
+        </Typography>
+        <Button
+          href='/contact-us.html'
+          className='bgRed white pl15 pr15 poppin upperCase'
+        >
+          Connect with Our Expert
+        </Button>
+      </section>
+      <LeadForm />
+      <Footer />
+    </div>
+  );
+}
