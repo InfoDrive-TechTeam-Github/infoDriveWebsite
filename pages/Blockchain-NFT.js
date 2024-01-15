fc7bc123acdadf4bdeb0b6eb856ae3ea1b99bc45
SAINATH <sachinpandey262523@gmail.com>
2024-01-15 16:52:32 +0530

    new

--- pages/Blockchain-NFT.js
+++ pages/Blockchain-NFT.js
@@ -1,35 +1,13 @@
-import * as React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Input from "@mui/material/Input";
-import InputLabel from "@mui/material/InputLabel";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Stack from "@mui/material/Stack";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Head from "next/head";
-import LeadForm from "../components/leadForm";
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 export default function blockchain() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <h1>html Not Found 404</h1>
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    // Redirect to the home page immediately
+    router.push("/"); // Redirect to the home page
+
+    // You can also use window.location.replace('/') if you prefer
+  }, []);
+
+  return null; // This page won't be visible, so return null
 }
