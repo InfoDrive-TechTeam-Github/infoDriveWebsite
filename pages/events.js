4d4dcb27eb9cb2072ff8589b8b79d9fd3532076d
alien2021brain <sachinpandey262523@gmail.com>
2024-05-09 16:59:33 +0530

    events added

---
+++ pages/events.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EventsComponent = () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <div>events</div>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default EventsComponent;
--- pages/index.js
+++ pages/index.js
@@ -136,18 +136,15 @@ export default function Index() {
           name="google-site-verification"
           content="7KEHJFp_iwMtPaWCOZumSwuNlb8Xjnd87er5I4VD71E"
         />
-        {/* <!-- Google Tag Manager -->     */}
-        <script>
-          {" "}
-          dangerouslySetInnerHTML=
-          {{
+        <script
+          dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PPS8R594');`,
           }}
-        </script>
+        />
 
         <script type="application/ld+json">
           {`
